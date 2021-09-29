4c1c1a3ac5a75f147485183aaefc3780b5f41312
Cleyton Silva <silva.rcleyton@gmail.com>
2021-09-29 18:27:10 -0300

    Create index action

--- app/controllers/api/v1/tools_controller.rb
+++ app/controllers/api/v1/tools_controller.rb
@@ -2,6 +2,12 @@ module Api
   module V1
     class ToolsController < ApplicationController
 
+      # List all tools
+      def index
+        @tools = Tool.all
+        render json: { status: 'Success' }, status: :ok
+      end
+
     end
   end
 end
