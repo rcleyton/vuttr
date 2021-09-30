ce40fe4965a4fa688d5a955f48b39db945c2f121
Cleyton Silva <silva.rcleyton@gmail.com>
2021-09-29 21:17:55 -0300

    Build create action

--- app/controllers/api/v1/tools_controller.rb
+++ app/controllers/api/v1/tools_controller.rb
@@ -5,8 +5,24 @@ module Api
       # List all tools
       def index
         @tools = Tool.all
-        render json: { status: 'Success' }, status: :ok
+        render json: @tools, status: :ok
       end
+
+      # Register a new tool
+      def create
+        @tool = Tool.new(tool_params)
+        if @tool.save
+          render json: @tool, status: :created
+        end
+      end
+
+      private
+
+      # Allowed parameters to create a new tool
+      def tool_params
+        params.require(:tool).permit(:title, :link, :description, :tags)
+      end
+
 
     end
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   namespace 'api' do
   	namespace 'v1' do
-      resources :tools
+      resources :tools, only: %i[ index create ]
   	end
   end
 end
