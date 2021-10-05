1b111167527981b9a937e7003a44d4afe9602276
Cleyton Silva <silva.rcleyton@gmail.com>
2021-10-05 13:48:52 -0300

    Search tool by tag

--- app/controllers/api/v1/tools_controller.rb
+++ app/controllers/api/v1/tools_controller.rb
@@ -8,6 +8,17 @@ module Api
         render json: @tools, status: :ok
       end
 
+      # Search tool by tag, params[:q] is the search parameter
+      def search
+        @tool = []
+        @tools = Tool.all.each do |tool|
+          if tool[:tags].include?(params[:q])
+            @tool.append(tool)
+          end
+        end
+        render json: @tool
+      end
+
       # Register a new tool
       def create
         @tool = Tool.new(tool_params)
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   namespace 'api' do
   	namespace 'v1' do
-      resources :tools, only: %i[ index create destroy ]
+      resources :tools, only: %i[ index create destroy ] do
+        get 'search', on: :collection
+      end
   	end
   end
 end
